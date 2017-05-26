a7695524928fce064241dfa5159850b847fb2c41
Eddie Sung <etsung2@Gmail.com>
2017-05-26 11:18:14 -0700

    Created components Home, Filter, DrinkList, DrinkDetail

---
+++ mixologylab/src/components/DrinkDetail.js
@@ -0,0 +1,18 @@
+/**
+ * Created by eddiemac on 5/26/2017.
+ */
+import React from 'react'
+import './DrinkDetail.css'
+
+class DrinkDetailPage extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Drink Detail Page</p>
+            </div>
+        )
+    }
+
+}
+
+export default DrinkDetailPage
