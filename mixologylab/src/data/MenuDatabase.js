f9a634f8a021220aa45000604c98c206b448dba5
Eddie Sung <etsung2@Gmail.com>
2017-06-06 18:54:51 -0700

    WIP(MenuSquares) - Add basic MenuSquares component

---
+++ mixologylab/src/data/MenuDatabase.js
@@ -0,0 +1,45 @@
+/**
+ * Created by eddiemac on 5/24/2017.
+ */
+module.exports = {
+    menu1 : {
+        key: 1,
+        name: 'Top Ten',
+        imageUrl: '/images/SpaceNeedle1.jpg',
+        textBackgroundColor: 'darkorange'
+    },
+    menu2 : {
+        key: 2,
+        name: 'Executive Orders',
+        imageUrl: '/images/SpaceNeedle2.jpg',
+        textBackgroundColor: 'darkgreen'
+    },
+    menu3 : {
+        key: 3,
+        name: 'Other Items',
+        imageUrl: '/images/SpaceNeedle3.jpg',
+        textBackgroundColor: 'darkseagreen'
+    },
+    menu4 : {
+        key: 4,
+        name: 'Blah 4',
+        imageUrl: '/images/SpaceNeedle4.jpg',
+        textBackgroundColor: ''
+    },
+    menu5 : {
+        key: 5,
+        name: 'Blah 5',
+        imageUrl: '/images/SpaceNeedle5.jpg',
+        textBackgroundColor: ''
+    },
+    menu6 : {
+        key: 6,
+        name: 'Blah 6',
+        imageUrl: '/images/SpaceNeedle6.jpg',
+        textBackgroundColor: ''
+    }
+
+
+
+
+}
