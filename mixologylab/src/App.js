be02ed457a56bcfdef1aae42786aec17bd233004
Eddie Sung <etsung2@Gmail.com>
2017-06-09 19:10:15 -0700

    Fix(fonts) - Add font link to index.html Add font to Header

--- mixologylab/src/App.js
+++ mixologylab/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Mixology Lab</h2>
+          <h1 id="title">Mixology Lab</h1>
             <div className="devNav">
                 <Link className="devLink" to="/">Home</Link>
                 <Link className="devLink" to="/customize">Filter</Link>
